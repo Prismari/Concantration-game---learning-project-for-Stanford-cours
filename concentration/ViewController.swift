d3ab5f6b081a869f106e55524aa02a9977653a8e
Мария Угорец <mariaugorec@gmail.com>
2019-08-30 11:49:02 +0300

    debug

--- concentration/ViewController.swift
+++ concentration/ViewController.swift
@@ -10,28 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var game: cardModel = cardModel(nbrOfCards: 8)
-    var imagesArray = ["😻", "🍑", "🏝", "🎈"]
+    
+    lazy var game: cardModel = cardModel(nbrOfCards: nbrOfCards)
+    
+    var nbrOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    
+    var flipCardCount = 0 {didSet {flipCardLabel.text = "flipped: \(flipCardCount)"} }
+    
+    @IBOutlet weak var flipCardLabel: UILabel!
     
     @IBOutlet var cardButtons: [UIButton]!
-
+    
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCardCount += 1
         if let CardNumber = cardButtons.index(of: sender) {
-            flipCard(withEmoji: imagesArray[CardNumber], on: sender)
+            game.chooseCard(at: CardNumber)
+            updateViewFromModel()
         }
         else {
             print ("error")
         }
     }
     
-    func flipCard (withEmoji emoji: String, on button: UIButton){
-        if button.currentTitle == emoji {
-            button.setTitle(" ", for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)
+    func updateViewFromModel () {
+        for index in cardButtons.indices {
+            let currentButton = cardButtons[index]
+            let currentCard = game.cardArray[index]
+            
+            if currentCard.isFlipped == true {
+                currentButton.setTitle(emoji(for: currentCard), for: UIControlState.normal)
+                currentButton.backgroundColor = #colorLiteral(red: 0, green: 0.7245563865, blue: 0.7277783751, alpha: 1)
+            } else {
+                currentButton.setTitle(" ", for: UIControlState.normal)
+                currentButton.backgroundColor = currentCard.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5401149988, blue: 0.8455886245, alpha: 1)
+            }
         }
-        else {
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 0, green: 0.7245563865, blue: 0.7277783751, alpha: 1)
+    }
+    
+    var imagesArray = ["😻", "🍑", "🏝", "🎈", "👙", "👗", "👑", "👠"]
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.indexNbr] == nil, imagesArray.count > 0 {
+            emoji[card.indexNbr] = imagesArray.remove(at: imagesArray.count.arc4andom)
+        }
+        if emoji[card.indexNbr] != nil { // can also write return emoji[card.indexNbr] ?? "?"
+            return emoji[card.indexNbr]!
+        } else {
+            return "?"
         }
     }
 }
+
+extension Int {
+    var arc4andom: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
+
+
+//TODO : add "private" to classes and vars - Lection 3 35.00
--- concentration/gemaCardsModel.swift
+++ concentration/gemaCardsModel.swift
@@ -11,20 +11,64 @@ import Foundation
 class cardModel
 {
     var cardArray = [Card]()
+    var indexFirstFlippedCardd: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cardArray.indices {
+                if cardArray[index].isFlipped == true {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+               
+            }
+            return foundIndex
+        }
+        set {
+            for index in cardArray.indices {
+                cardArray[index].isFlipped = (index == newValue)
+            }
+        }
+    }
+    
+    func chooseCard(at index: Int) {
+        if !cardArray[index].isMatched {
+
+            if let indexMatch = indexFirstFlippedCardd, indexMatch != index {
+                
+                if cardArray[index].indexNbr == cardArray[indexMatch].indexNbr {
+                    cardArray[index].isMatched = true
+                    cardArray[indexMatch].isMatched = true
+                }
+                cardArray[index].isFlipped = true
+                
+            } else {
+                indexFirstFlippedCardd = index
+            }
+        }
+    }
     
     init (nbrOfCards: Int) {
         for identifier in 0..<nbrOfCards {
             let newCard = Card(identifier)
             cardArray += [newCard, newCard]
         }
-        for identifier in 0..<nbrOfCards {
+        for card in cardArray {
+            print(card.indexNbr)
+        }
+        print ("lets random")
+        for identifier in 0..<(nbrOfCards * 2) {
+            let randomIndex = Int.random(in: 0..<(nbrOfCards * 2))
             let box = cardArray[identifier]
-            var randomIndex = Int(arc4random())
-            while randomIndex > nbrOfCards {
-                randomIndex = Int(arc4random())
-            }
-            cardArray[randomIndex] = cardArray[identifier]
+            cardArray[identifier] = cardArray[randomIndex]
             cardArray[randomIndex] = box
         }
+        for card in cardArray {
+            print(card.indexNbr)
+        }
     }
+    
+    
 }
